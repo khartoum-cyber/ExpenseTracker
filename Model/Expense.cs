713c49f294e10520e5742518b6fc5de9c8515abd
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-06-10 10:32:32 +0200

    added expense model

---
+++ Model/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Model
+{
+    internal class Expense
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
